3bdf87bf3e82f4320866531a0137ff18aa697541
divaprates <divasilvaprates@gmail.com>
2023-09-23 23:42:05 -0300

    refactor: organizando pastas

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ComponentsModule } from '../components/components.module';
+import { ComponentsModule } from '../shared/components.module';
 import { CardapioComponent } from './cardapio/cardapio.component';
 import { HomeComponent } from './home/home.component';
 import { QuemSomosComponent } from './quem-somos/quem-somos.component';
--- src/app/shared/components.module.ts
+++ src/app/shared/components.module.ts
@@ -24,6 +24,7 @@ import { MenuComponent } from './menu/menu.component';
     FormsModule
   ],
   exports: [
+    ButtonComponent,
     CardComponent,
     MenuComponent,
     HeaderComponent,
